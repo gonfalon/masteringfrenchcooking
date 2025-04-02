a69af944e49135f292ec6bf863aef175127c5faa
Ben <dunphy1996@gmail.com>
2025-04-02 06:06:03 -0400

    Start on server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+import express from 'express';
+import tesseract from 'tesseract.js';
+
+const port = 75;
+const app = express();
+
+app.get('/ping', (_, res) => {
+    res.sendStatus(200);
+});
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+
+console.log('Server running');
