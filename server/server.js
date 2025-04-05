323c587edae22d233baafb4d53318624cdf1afa5
gonfalon <dunphy1996@gmail.com>
2025-04-05 08:10:24 -0400

    Work

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import tesseract from 'tesseract.js';
 
-const port = 75;
+const port = 2369;
 const app = express();
 
+// enable Cross Origin Resource Sharing
+app.use(cors());
+
+// enable logging
+app.use(morgan('dev'));
+
+// enable parsing form data
+app.use(express.urlencoded({ extended: true }));
+
+// ping endpoint for server existence check
 app.get('/ping', (_, res) => {
-    res.sendStatus(200);
+    res.send('200');
 });
 
+// upload endpoint
+app.post('/upload', async (req, res) => {
+    res.send("yay!")
+});
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`listening on port ${port}`);
 });
-
-console.log('Server running');
