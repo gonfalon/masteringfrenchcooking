a0905d84125f4108eb825f656fcb075749afb6d5
gonfalon <dunphy1996@gmail.com>
2025-04-21 08:48:38 -0400

    Adjusting approach

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
 const express  = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
-const multer = require('multer');
-const { createWorker } = require('tesseract.js');
 
 const port = 2369;
 const app = express();
 
-// enable Cross Origin Resource Sharing
-app.use(cors());
-
-// enable file uploads. Stores the files in the default temp directory
-const upload = multer();
+// expose files in public on root.
+app.use(express.static('public'));
 
 // enable logging
 app.use(morgan('dev'));
@@ -25,31 +19,14 @@ app.get('/ping', (_, res) => {
 });
 
 // upload endpoint
-const imageFieldNames = upload.fields([{name: 'image'}, {name: 'image2'}]);
-app.post('/upload', imageFieldNames, async (req, res) => {
+app.post('/api/add', imageFieldNames, async (req, res) => {
     console.log(req.body);
-    console.log(req.files);
-    
-    const category = req.body.category;
+
     const title = req.body.title;
-    const image = req.files.image;
-    const image2 = req.files?.image2;
-
-    // set up tesseract worker
-    const worker = await createWorker('eng');
-    const image1Text = await worker.recognize(image.buffer);
-    console.log(image1Text);
-
-    res.send(image1Text);
-    await worker.terminate();
+    const category = req.body.category;
+    
+    res.send("yay!");
 });
-
-/*
-// create https server
-const httpsServer = https.createServer({
-    key: fs.readFileSync('selfsigned.key'),
-    cert: fs.readFileSync('selfsigned.crt')
-}, app);*/
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
