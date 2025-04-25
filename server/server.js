09573d6151d5305e41918e0d077851d2fa553e3f
gonfalon <dunphy1996@gmail.com>
2025-04-24 21:17:43 -0400

    Adding and viewing recipies working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
+//#region imports
 const express  = require('express');
 const morgan = require('morgan');
+const ejs = require('ejs');
+const sqlite = require('sqlite3');
+const database = new sqlite.Database('./recipies.db');
+//#endregion
+
+//#region setup
+// create database table if not exists
+database.run(`
+    CREATE TABLE IF NOT EXISTS recipies (
+        slug TEXT PRIMARY KEY,
+        title TEXT,
+        category TEXT,
+        ingredients TEXT,
+        instructions TEXT
+    )
+`);
 
 const port = 2369;
 const app = express();
 
-// expose files in public on root.
+// expose files in ./public on root.
 app.use(express.static('public'));
 
 // enable logging
@@ -13,22 +30,49 @@ app.use(morgan('dev'));
 // enable parsing form data
 app.use(express.urlencoded({ extended: true }));
 
-// ping endpoint for server existence check
-app.get('/ping', (_, res) => {
-    res.send('200');
-});
+// set up ejs templating
+app.set('view engine', 'ejs');
+//#endregion
+
+//#region endpoints
 
 // upload endpoint
-app.post('/api/add', imageFieldNames, async (req, res) => {
-    console.log(req.body);
+app.post('/api/add', async (req, res) => {
 
+    console.log(req.body);
     const title = req.body.title;
     const category = req.body.category;
-    
-    res.send("yay!");
+    const ingredients = req.body.ingredients;
+    const instructions = req.body.instructions;
+
+    // generate a slug converting title to kebab case
+    const slug = title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+
+    database.run(
+        `INSERT INTO recipies (slug, title, category, ingredients, instructions) VALUES (?, ?, ?, ?, ?)`,
+        [slug, title, category, ingredients, instructions]
+    );
+
+    //redirect to slug
+    res.redirect(`/${slug}`);
 });
+
+// slug endpoint
+app.get('/:slug', async (req, res) => {
+    const slug = req.params.slug;
+    database.get(`SELECT * FROM recipies WHERE slug = ?`, [slug], (err, recipie) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+        }
+        res.render('recipie', recipie);
+    });
+});
+
+//#endregion
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
-    //console.log(networkInterfaces());
+}).on('error', (err) => {
+    console.error(err);
 });
